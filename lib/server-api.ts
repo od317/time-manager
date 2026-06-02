c6904e01cc1ca1ba9114dc9bacd348fd98d48350
osama drbas <osamadrbas31@gmail.com>
2026-06-02 05:09:14 +0300

    edit: testing env on production

--- lib/server-api.ts
+++ lib/server-api.ts
@@ -1,5 +1,6 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
-
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://timeflow-api.onrender.com/api";
+  
 interface FetchOptions<TParams = Record<string, unknown>> {
   params?: TParams;
   revalidate?: number | false;
